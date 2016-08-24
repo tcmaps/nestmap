854d37cea7ee6aed8e47fa879b8ddeab8e3d2009
Tr4sHCr4fT <Tr4sHCr4fT@users.noreply.github.com>
2016-08-24 21:21:22 +0200

    csv export

--- analyze.py
+++ analyze.py
@@ -1,21 +1,54 @@
 #!/usr/bin/env python
 
-import os, sys, sqlite3
+import sqlite3, sys
 from s2sphere import CellId
+from utils import get_pokenames
 
+def gen_db(dbfile):
+
+    dbin = sqlite3.connect(dbfile)
+    dbout = sqlite3.connect(':memory:'); dbcur = dbout.cursor()
+    dbcur.execute("CREATE TABLE encount (spawn VARCHAR PRIMARY KEY, poke INT, count INT DEFAULT 0)")
+    
+    for i in range(151):
+        
+        encs = [x[0] for x in dbin.cursor().execute("SELECT spawn_id FROM encounters "
+                        "WHERE spawn_id IS NOT NULL AND pokemon_id = %d" % i).fetchall()]
+        
+        if len(encs) > 0:
+        
+            
+            for e in encs:
+                dbcur.execute("INSERT OR IGNORE INTO encount (spawn, poke) VALUES ('{}',{})".format(e,i))
+                dbcur.execute("UPDATE encount SET count = count + 1 WHERE spawn = '%s'" % e)
+                
+    dbout.commit()
+    return dbout
+
+def gen_csv(db, filename):
+    
+    f = open(filename,'w')
+    pname = get_pokenames('pokes.txt')
+    f.write('spawn_id, pokemon_id, pokemon_name, count, latitude, longitude')
+    
+    for i in range(151):
+    
+        spwns = db.cursor().execute("SELECT spawn, count FROM encount "
+                        "WHERE poke = %d AND count > 1 ORDER BY count DESC" % i).fetchall()
+        
+        if len(spwns) > 0:
+            for s,c in spwns:
+                _ll = CellId.from_token(s).to_lat_lng()
+                lat, lng, = _ll.lat().degrees, _ll.lng().degrees
+                f.write("{},{},{},{},{},{}\n".format(s,i,pname[i],c,lat,lng))
+                
+    print('Done!')
+
+def main():
+    db = gen_db('db2.sqlite')
+    
+    if sys.argv[1] == 'export' and sys.argv[2] == 'csv':
+        gen_csv(db, sys.argv[3])
+    
 if __name__ == '__main__':
-    #if os.path.isfile('nests.sqlite'): os.remove('nests.sqlite')
-    dbin = sqlite3.connect(sys.argv[1])
-    #dbout = sqlite3.connect('nests.sqlite')
-    
-    encs = dbin.cursor().execute("SELECT encounter_id, spawn_id, pokemon_id FROM encounters WHERE spawn_id IS NOT NULL ").fetchall()
-    
-    f = open(sys.argv[2],'w')
-    f.write('latitude,longitude,pokemon\n')
-    for enc in encs:
-        cell = CellId.from_token(enc[1]);_ll = CellId.to_lat_lng(cell)
-        lat, lng, = _ll.lat().degrees, _ll.lng().degrees
-        f.write("{},{},{}\n".format(lat, lng, enc[2]))
-
-    f.flush(); f.close()
-    print('Donezo!')
+    main()
