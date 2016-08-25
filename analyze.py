8b29d9492816b5053e73a1bec2e457a89d909e25
Tr4sHCr4fT <Tr4sHCr4fT@users.noreply.github.com>
2016-08-25 15:09:52 +0200

    readded count function

--- analyze.py
+++ analyze.py
@@ -4,7 +4,7 @@ import sqlite3, sys
 from s2sphere import CellId
 from utils import get_pokenames
 
-def gen_db(dbfile):
+def gen_csv_counted(filename, dbfile):
 
     dbin = sqlite3.connect(dbfile)
     dbout = sqlite3.connect(':memory:'); dbcur = dbout.cursor()
@@ -21,37 +21,55 @@ def gen_db(dbfile):
             
             for e in encs:
                 dbcur.execute("INSERT OR IGNORE INTO encount (spawn, poke) VALUES ('{}',{})".format(e,i))
-                dbcur.execute("UPDATE encount SET count = count + 1 WHERE spawn = '%s'" % e)
+                dbcur.execute("UPDATE encount SET count = count + 1 WHERE spawn = '%s' AND poke = %d" % (e,i))
                 
     dbout.commit()
-    return dbout
+        
+    f = open(filename,'w')
+    pname = get_pokenames('pokes.txt')
+    f.write('spawn_id, latitude, longitude, count, pokemon_id, pokemon_name\n')
 
-def gen_csv(filename):
+    spwns = dbout.execute("SELECT spawn, poke, count FROM encount ORDER BY poke ASC, count DESC").fetchall()
+    
+    if len(spwns) > 0:
+        for s,p,c in spwns:
+            _ll = CellId.from_token(s).to_lat_lng()
+            lat, lng, = _ll.lat().degrees, _ll.lng().degrees
+            f.write("{},{},{},{},{},{}\n".format(s,lat,lng,c,p,pname[p]))
+                
+    print('Done!')
+
+def gen_csv(filename, dbfile):
     
     f = open(filename,'w')
-    db = sqlite3.connect('db2.sqlite')
+    db = sqlite3.connect(dbfile)
     pname = get_pokenames('pokes.txt')
-    f.write('spawn_id, pokemon_id, pokemon_name, latitude, longitude\n')
+    f.write('spawn_id, latitude, longitude, pokemon_id, pokemon_name\n')
 
     spwns = db.execute("SELECT spawn_id, pokemon_id FROM encounters WHERE spawn_id IS NOT NULL").fetchall()
     
     if len(spwns) > 0:
-        for s,i in spwns:
+        for s,p in spwns:
             _ll = CellId.from_token(s).to_lat_lng()
             lat, lng, = _ll.lat().degrees, _ll.lng().degrees
-            f.write("{},{},{},{},{}\n".format(s,i,pname[i],lat,lng))
+            f.write("{},{},{},{},{}\n".format(s,lat,lng,p,pname[p]))
                 
     print('Done!')
 
 def main():
-    #db = gen_db('db2.sqlite')
+    dbfilename = 'db2.sqlite'
     
-    if len(sys.argv)<4:
-        print('currently only supported command: analyze.py export csv <filename>')
+    if len(sys.argv)<=1:
+        print('currently supported commands:\n')
+        print('analyze.py export csv <filename>')
+        print('analyze.py export csv count <filename>')
         return
     
     if sys.argv[1] == 'export' and sys.argv[2] == 'csv':
-        gen_csv(sys.argv[3])
+        if sys.argv[3] == 'count':
+            gen_csv_counted(sys.argv[4],dbfilename)
+        else:
+            gen_csv(sys.argv[3],dbfilename)
     
 if __name__ == '__main__':
     main()
